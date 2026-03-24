553422936410915d25a90cd73c4e00d95f4f25f0
nguyen duc <nguyenduc@MacBook-Pro-cua-nguyen.local>
2026-03-24 11:45:40 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -86,6 +86,6 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
 });
 
 const PORT = process.env.PORT || 4000;
-server.listen(PORT, () => {
-  console.log(`🚀 Server is running on port ${PORT}`);
+server.listen(Number(PORT), '0.0.0.0', () => {
+  console.log(`🚀 Server is running on port ${PORT} (mapped to all interfaces)`);
 });
